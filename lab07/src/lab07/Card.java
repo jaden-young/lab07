249bcecda80033c976048d6da58163a2bc7d661d
jaden <jaden@HAL-9000>
2015-03-26 22:00:15 -0500

    Let's hope to god that all of these enums work out alright

--- lab07/src/lab07/Card.java
+++ lab07/src/lab07/Card.java
@@ -1,79 +1,82 @@
 package lab07;
 
 /**
- * This class represents a standard playing card. Suit is an option between
- * C for clubs, D for diamonds, etc for the 4 suits. The value of the card is 
- * represented as an integer 2-10, or the first capital letter of face cards,
- * J for jack, A for ace, etc. Objects are immutable. Accessor methods for the 
- * two instance variables are provided.
+ * This class represents a standard playing card. Enums are used for suits and 
+ * card values. Suits are hearts, clubs, spades, and diamonds. 
+ * Values are two - ten, jack, queen, king, ace. Each has an integer value 
+ * associated with it to store the actual card rank (2 is 2, up to ace 
+ * being 14). Objects are immutable.
  * @author Jaden Young
  */
 public class Card {
-    private char suit;
-    private String value;
+    private Suits suit;
+    private Values value;
     
-    public Card(char startSuit, String startValue) throws 
-            IllegalArgumentException {
-        setSuit(startSuit);
-        setValue(startValue);
+	public enum Suits {
+		SPADES,
+		HEARTS,
+		DIAMONDS,
+		CLUBS;
+	}
+	
+	public enum Values {
+		TWO(2),
+		THREE(3),
+		FOUR(4),
+		FIVE(5),
+		SIX(6),
+		SEVEN(7),
+		EIGHT(8),
+		NINE(9),
+		TEN(10),
+		JACK(11),
+		QUEEN(12),
+		KING(13),
+		ACE(14);
+		
+		private final int cardValue;
+		
+		private Values(int newValue) {
+			cardValue = newValue;
+		}
+		
+		public int getCardValue() {
+			return cardValue;
+		}
+	}
+	
+	/**
+	 * Creates a new card with a Suit and a card Value
+	 * @param startSuit Suit for the card - Spades, hearts, diamonds, or clubs
+	 * @param startValue value of the card
+	 */
+    public Card(Suits startSuit, Values startValue) {
+        suit = startSuit;
+		value = startValue;
+		
     }
     
     /** Returns the suit of the card */
-    public char getSuit() {
+    public Suits getSuit() {
         return suit;
     }
     
     /** Returns the value of the card (2-10, J, Q, K, A) */
-    public String getValue() {
+    public Values getValue() {
         return value;
     }
     
     /**
      * Returns a string representation of the card showing the value and suit
      * Examples:
-     *          10 of spades - 10S
-     *          6 of clubs - 6C
-     *          Jack of diamonds - JD
-     *          King of hearts - KH
+     *          10 of Spades
+     *          6 of Clubs
+     *          Jack of Diamonds
+     *          King of Hearts
      * @return Value and suit of the card
      */
+	@Override
     public String toString() {
-        return value + suit;
-    }
-    
-    
-    //---------------- private methods -------------------------
-    private void setSuit(char xSuit) {
-        switch (xSuit) {
-            case 'C':
-            case 'D':
-            case 'H':
-            case 'S':
-                suit = xSuit;
-            default:
-                throw new IllegalArgumentException("That is not a valid suit. "
-                        + "Possible options are: \nC, D, H, S");
-        }
-    }
-    private void setValue(String xValue) {
-        switch (xValue) {
-            case "2":
-            case "3":
-            case "4":
-            case "5":
-            case "6":
-            case "7":
-            case "8":
-            case "9":
-            case "10":
-            case "J":
-            case "Q":
-            case "K":
-            case "A":
-                value = xValue;
-            default:
-                throw new IllegalArgumentException("That is not a valid card "
-                        + "value. Possible options: \nIntegers 2-10, J, Q, K, A");
-        }
+        return value + " of " + suit;
     }
 }
