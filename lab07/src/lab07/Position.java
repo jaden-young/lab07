0b5d22417e16328c2a7cd0316ab564c2c04486c0
jaden <jaden@HAL-9000>
2015-03-26 19:07:50 -0500

    Commented Position interface

--- lab07/src/lab07/Position.java
+++ lab07/src/lab07/Position.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package lab07;
 
 /**
- *
- * @author jaden.young
+ * Position interface defined in the book.
+ * @author Jaden Young
  */
 public interface Position<E> {
     /**
